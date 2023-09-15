fb0f317733cb3068bc11f099c33303b57d9d4e08
berrylafci <101870168+xemeriusss@users.noreply.github.com>
2023-09-15 14:32:15 +0300

    Add files via upload

---
+++ CSE222 - Data Structures And Algorithms/HW3/hw3_ll/hw3_ll/Message.java
@@ -0,0 +1,124 @@
+package hw3_ll;
+
+/**
+ * Message class is used to create messages.
+ * @version 1.0 05.04.2023
+ * @author  Berru Lafci
+ */
+public class Message {
+    
+    private int messageId;
+    private int senderId;
+    private int receiverId;
+    private String content;
+    private String senderUsername;
+    private String receiverUsername;
+
+    /**
+     * Constructor for Message class.
+     * @param messageId message id of the message 
+     * @param content content of the message 
+     */
+    public Message(int messageId, String content) {
+        this.messageId = messageId;
+        this.content = content;
+    }
+
+    /**
+     * Getter returns the messageId of the message.
+     * @return The messageId of the message.
+     */
+    public int getMessageId() {
+        return messageId;
+    }
+
+    /**
+     * Getter returns the senderId of the message.
+     * @return The senderId of the message.
+     */
+    public int getSenderId() {
+        return senderId;
+    }
+
+    /**
+     * Getter returns the receiverId of the message.
+     * @return The receiverId of the message.
+     */
+    public int getReceiverId() {
+        return receiverId;
+    }
+
+    /**
+     * Getter returns the content of the message.
+     * @return The content of the message.
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * Getter returns the username of sender of the message.
+     * @return The content of the message.
+     */
+    public String getSenderUsername() {
+        return senderUsername;
+    }
+
+    /**
+     * Getter returns the username of receiver of the message.
+     * @return The content of the message.
+     */
+    public String getReceiverUsername() {
+        return receiverUsername;
+    }
+
+    /**
+     * Setter to set the message ID
+     * @param messageId The message ID to be set.
+     */
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    /**
+     * Setter to set the sender ID
+     * @param senderId The sender ID to be set.
+     */
+    public void setSenderId(int senderId) {
+        this.senderId = senderId;
+    }
+
+    /**
+     * Setter to set the receiver ID
+     * @param receiverId The receiver ID to be set.
+     */
+    public void setReceiverId(int receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    /**
+     * Setter to set the content
+     * @param content The content to be set.
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * Setter to set the sender username
+     * @param senderUsername The sender username to be set.
+     */
+    public void setSenderUsername(String senderUsername) {
+        this.senderUsername = senderUsername;
+    }
+
+    /**
+     * Setter to set the receiver username
+     * @param receiverUsername The receiver username to be set.
+     */
+    public void setReceiverUsername(String receiverUsername) {
+        this.receiverUsername = receiverUsername;
+    }
+
+
+}
---
+++ CSE222 - Data Structures And Algorithms/HW3/hw3_ll/hw3_ll/testclass1.java
@@ -0,0 +1,68 @@
+package hw3_ll;
+
+public class testclass1 {
+    public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
+
+        
+        Account gizemsungu = new Account(1,"gizemsungu", "13.01.2000", "Istanbul");
+        Account sibelgulmez = new Account(2,"sibelgulmez", "01.01.2000", "Tokyo");
+        Account gokhankaya = new Account(3, "gokhankaya", "02.02.2000", "Seoul");
+
+
+        sibelgulmez.login();
+
+        Post post1 = new Post(1, "Tokyo is a great city!");
+        Post post2 = new Post(2, "Tokyo is capital of Japan!");
+
+        sibelgulmez.addPost(post1);
+        sibelgulmez.addPost(post2);
+
+        sibelgulmez.followAccount(gokhankaya);
+        sibelgulmez.followAccount(gizemsungu);
+
+        sibelgulmez.logout();
+
+
+        gokhankaya.login();
+
+        gokhankaya.viewProfile(sibelgulmez);
+        gokhankaya.viewPosts(sibelgulmez);
+        gokhankaya.likePost(post1, 1, sibelgulmez);
+        gokhankaya.commentPost(post1, 1, sibelgulmez, "Yes, I love Tokyo too!");
+        gokhankaya.followAccount(sibelgulmez);
+        gokhankaya.followAccount(gizemsungu);
+
+        Message message1 = new Message(1, "Hello, how are you?");
+
+        gokhankaya.sendMessage(gizemsungu, message1);
+
+        gokhankaya.logout();
+
+
+        gizemsungu.login();
+
+        gizemsungu.checkOutbox();
+        gizemsungu.checkInbox();
+
+        gizemsungu.viewInbox();
+        
+        gizemsungu.viewProfile(sibelgulmez);
+        gizemsungu.viewPosts(sibelgulmez);
+        gizemsungu.viewInteractions(sibelgulmez);
+        gizemsungu.likePost(post2, 2, sibelgulmez);
+        gizemsungu.viewInteractions(sibelgulmez);
+/* 
+        gokhankaya.login();
+        gokhankaya.showHistory();
+        gizemsungu.showHistory();  */
+
+        long endTime = System.nanoTime();
+        double nanoseconds = (endTime - startTime);
+        double seconds = nanoseconds / 1000000000.0;
+        System.out.println("Duration: " + seconds + " seconds");
+        
+  
+    }
+}
